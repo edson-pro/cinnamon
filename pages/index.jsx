557c29be1178d8f73cd153d1f665fb9d92555c4d
ntwali edson <ntwaliedson9@gmail.com>
2022-11-03 20:38:58 +0200

    sx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,6 @@ import Button from "../components/ui/Button";
 import AgileTeam from "../components/sections/AgileTeam";
 import Blog from "../components/sections/Blog";
 import College from "../components/sections/College";
-import Hero from "../components/sections/hero";
 import Offices from "../components/sections/Offices";
 import Positions from "../components/sections/Positions";
 import Products from "../components/sections/Products";
@@ -13,6 +12,7 @@ import Services from "../components/sections/Services";
 import Testimonies from "../components/sections/Testimonies";
 import WhoWeAre from "../components/sections/WhoWeAre";
 import WorkTogether from "../components/sections/WorkTogether";
+import Hero from "../components/sections/hero";
 export default function Home() {
   return (
     <Fragment>
